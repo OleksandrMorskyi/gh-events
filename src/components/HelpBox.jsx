99010cb7947a8cc85911034f0379faf22c5a85bc
AlexMorskyi <Alex.capone35@gmail.com>
2024-05-02 09:49:22 +0200

    changed workflow

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,8 +1,7 @@
 import { string } from 'prop-types';
 
 import './HelpBox.css';
-
-function HelpBox({ title, text }) {
+ function HelpBox({ title, text }) {
   return (
     <article className="help-box">
       <h2>{title}</h2>
